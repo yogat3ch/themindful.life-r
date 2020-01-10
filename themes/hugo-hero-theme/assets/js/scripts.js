55eb8b0ea0962fea76be1b6ff646ec5151121dc3
Stephen Synchronicity <sholsen@alumni.emory.edu>
2020-01-09 19:04:45 -0500

    Adjust position of logo and hamburger (and size of hb), JS rollout menu scripts.js:  - Hamburger has mouseOver and mouseLeave that rollout menu for screen widths above 72 and keep it rolled out as long as mouse is in header _hamburger.scss:  - remove display non breakpoint so hamburger always shows (as on main)  - adjust margins to match main  - add important to black to override order of css file loading _header.scss:  - change height to a % value for consistent placement above mid bps _hero-image.scss:  - remove margin-bottom _logo.scss:  - adjust margins for consistent appearance according to bps _main_menu.scss:  - Show above mid bp hamburgers.scss  - Adjust padding, spacing and width to match main site

--- themes/hugo-hero-theme/assets/js/scripts.js
+++ themes/hugo-hero-theme/assets/js/scripts.js
@@ -1,9 +1,22 @@
 var body = document.querySelector('body')
 var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
 var menuContainer = document.querySelector('#main-menu-mobile');
+var header = document.querySelector(".header-absolute")
 
 menuTrigger.onclick = function() {
     menuContainer.classList.toggle('open');
     menuTrigger.classList.toggle('is-active')
     body.classList.toggle('lock-scroll')
+}
+if (screen.width > 720) {
+    menuTrigger.addEventListener("mouseover", mouseOver)
+    header.addEventListener("mouseleave", mouseLeave)
+
+    function mouseOver() {
+        document.getElementById("main-menu").style.display = "block";
+        document.getElementById("main-menu").style.transition = "all .25s ease-in-out 0;"
+    }
+    function mouseLeave() {
+        document.getElementById("main-menu").style.display = "none";
+    }   
 }
